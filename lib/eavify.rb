e7395947c06890108b0a363ef811cbb0c7ac11d5
bugloper <bugloper@gmail.com>
2025-01-06 18:57:42 +0600

    refactor

--- lib/eavify.rb
+++ lib/eavify.rb
@@ -4,7 +4,6 @@ module Eavify
   extend ActiveSupport::Concern
 
   included do
-    class_attribute :scope_fields, default: {}
     validate :validate_scope_fields
   end
 
@@ -15,52 +14,70 @@ module Eavify
         fields = cfg[:fields]
         validations = cfg[:validations] || {}
 
-        scope_fields[scope] = {
-          fields: fields,
-          validations: validations
-        }
-
         scope scope, lambda {
           select(
             'id',
             'scope',
             'created_at',
             'updated_at',
-            *fields.map do |field, data_type|
-              case data_type
+            *fields.map do |field, datatype|
+              case datatype
               when :text, :varchar
-                "(cols->>'#{field}')::text as #{field}"
+                "(fields->>'#{field}')::text as #{field}"
               when :decimal, :numeric
-                "(cols->>'#{field}')::decimal as #{field}"
+                "(fields->>'#{field}')::decimal as #{field}"
               when :integer
-                "(cols->>'#{field}')::integer as #{field}"
+                "(fields->>'#{field}')::integer as #{field}"
               when :float, :double
-                "(cols->>'#{field}')::float as #{field}"
+                "(fields->>'#{field}')::float as #{field}"
               when :boolean
-                "(cols->>'#{field}')::boolean as #{field}"
+                "(fields->>'#{field}')::boolean as #{field}"
               when :date
-                "(cols->>'#{field}')::date as #{field}"
+                "(fields->>'#{field}')::date as #{field}"
               when :timestamp
-                "(cols->>'#{field}')::timestamp as #{field}"
+                "(fields->>'#{field}')::timestamp as #{field}"
               when :timestamptz
-                "(cols->>'#{field}')::timestamptz as #{field}"
+                "(fields->>'#{field}')::timestamptz as #{field}"
               when :uuid
-                "(cols->>'#{field}')::uuid as #{field}"
+                "(fields->>'#{field}')::uuid as #{field}"
               else
-                "(cols->>'#{field}')::text as #{field}"
+                "(fields->>'#{field}')::text as #{field}"
               end
             end
           ).where(scope: scope)
         }
 
-        fields.each do |field, _data_type|
+        fields.each do |field, _|
           field_name = field.to_s
           define_method(field_name) do
-            cols[field_name]
+            fields[field_name]
           end
 
           define_method("#{field_name}=") do |value|
-            cols[field_name] = value
+            fields[field_name] = value
+          end
+        end
+
+        define_validations_for(scope, fields, validations)
+      end
+    end
+
+    private
+
+    def define_validations_for(scope, fields, validations)
+      validations.each do |validation_type, field_list|
+        Array(field_list).each do |field|
+          next unless fields.keys.include?(field.to_s)
+
+          case validation_type
+          when :presence
+            validates_presence_of field
+          when :numericality
+            validates_numericality_of field
+          when :length
+            validates_length_of field, maximum: field_list[field.to_s][:maximum], message: field_list[field.to_s][:message]
+          when :inclusion
+            validates_inclusion_of field, in: field_list[field.to_s][:in], message: field_list[field.to_s][:message]
           end
         end
       end
@@ -70,24 +87,7 @@ module Eavify
   private
 
   def validate_scope_fields
-    scope_data = self.class.scope_fields[scope.to_sym]
-    return unless scope_data
-
-    scope_data[:validations].each do |validation_type, fields|
-      Array(fields).each do |field|
-        next unless scope_data[:fields].keys.include?(field.to_s)
-
-        case validation_type
-        when :presence
-          validates_presence_of field
-        when :numericality
-          validates_numericality_of field
-        when :length
-          validates_length_of field, maximum: fields[field][:maximum], message: fields[field][:message]
-        when :inclusion
-          validates_inclusion_of field, in: fields[field][:in], message: fields[field][:message]
-        end
-      end
-    end
+    # Implement 
+    true
   end
 end
